04c26b4306ca52307ed374dbc9306adb4e33f0c0
Radek <tukedarr@gmail.com>
2018-11-08 18:53:19 +0100

    Dodanie Kroków, podscenariusza i scenariusza głównego oraz konfiguracja pom.xml

---
+++ src/main/java/pl/put/poznan/transformer/app/Krok.java
@@ -0,0 +1,25 @@
+public class Krok {
+    String aktor;
+    String wiersz;
+
+    public Krok(String aktor, String wiersz) {
+        this.aktor = aktor;
+        this.wiersz = wiersz;
+    }
+
+    public String getAktor() {
+        return aktor;
+    }
+
+    public String getWiersz() {
+        return wiersz;
+    }
+
+    public void setAktor(String aktor) {
+        this.aktor = aktor;
+    }
+
+    public void setWiersz(String wiersz) {
+        this.wiersz = wiersz;
+    }
+}
---
+++ src/main/java/pl/put/poznan/transformer/app/Podscenariusz.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Podscenariusz extends ScenariuszGlowny{
+
+    int zagniezdzenie;
+    List<Krok> listaKrokow = new ArrayList();
+    int liczbaKrokow;
+    String slowoKlucz;
+
+    public Podscenariusz(int zagniezdzenie, Krok listaKrokow, int liczbaKrokow, String slowoKlucz) {
+        super();
+        this.zagniezdzenie = zagniezdzenie;
+        //this.listaKrokow = listaKrokow;
+        this.listaKrokow = new ArrayList<>();
+        getListaKrokow().add(listaKrokow);
+        this.liczbaKrokow = liczbaKrokow;
+        this.slowoKlucz = slowoKlucz;
+    }
+    public Podscenariusz() {
+        super();
+    }
+
+
+
+    public int getZagniezdzenie() {
+        return zagniezdzenie;
+    }
+
+    public List<Krok> getListaKrokow() {
+        return listaKrokow;
+    }
+
+    public int getLiczbaKrokkow() {
+        return liczbaKrokow;
+    }
+
+    public String getSlowoKlucz() {
+        return slowoKlucz;
+    }
+
+    public void setZagniezdzenie(int zagniezdzenie) {
+        this.zagniezdzenie = zagniezdzenie;
+    }
+
+    public void setListaKrokow(List<Krok> listaKrokow) {
+        this.listaKrokow = listaKrokow;
+    }
+
+    public void setLiczbaKrokkow(int liczbaKrokkow) {
+        this.liczbaKrokow = liczbaKrokkow;
+    }
+
+    public void setSlowoKlucz(String slowoKlucz) {
+        this.slowoKlucz = slowoKlucz;
+    }
+}
