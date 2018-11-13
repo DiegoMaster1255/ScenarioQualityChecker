e40d19c6565edbde80589420b9a21d75af55a12a
Raffel222 <Raffel222@gmail.com>
2018-11-14 00:34:25 +0100

    komentarze do dokumentacji klasy Krok

--- src/main/java/pl/put/poznan/transformer/app/Krok.java
+++ src/main/java/pl/put/poznan/transformer/app/Krok.java
@@ -1,34 +1,56 @@
 package pl.put.poznan.transformer.app;
 
 public class Krok {
+    /**Aktor uczestniczacy w kroku.*/
     public String aktor;
+    /**Pojedynczy krok.*/
     public String wiersz;
 
+    /** Konstruktor domyslny kroku.
+     *
+     */
     public Krok(){}
 
-
+    /**Konstruktor kroku z aktorem i wierszem.
+     * @param aktor aktor uczestniczacy w kroku.
+     * @param wiersz tresc kroku.
+     */
     public Krok(String aktor, String wiersz) {
         this(wiersz);
         this.aktor = aktor;
     }
 
+    /** Konstruktor kroku bez aktora.
+     * @param wiersz tresc kroku.
+     */
     public Krok( String wiersz){
         this.wiersz = wiersz;
         this.aktor = "";
     }
-
+    /** Zwraca aktora, zawartego w kroku.
+     * @return aktor
+     * */
     public String getAktor() {
         return aktor;
     }
 
+    /** Zwraca tresc kroki.
+     * @return wiersz
+     */
     public String getWiersz() {
         return wiersz;
     }
 
+    /** Ustala aktora, wykonawce kroku.
+     * @param aktor aktor uczestniczacy w kroku.
+     */
     public void setAktor(String aktor) {
         this.aktor = aktor;
     }
 
+    /** Ustala tresc kroki.
+     * @param wiersz tresc kroku.
+     */
     public void setWiersz(String wiersz) {
         this.wiersz = wiersz;
     }
